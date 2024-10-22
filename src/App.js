f85de5906793799579062003e9413f95fa794b0b
hienjoy <wisdom16no@naver.com>
2024-10-22 20:22:10 +0900

    feat: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
