60cfd6205876bfa5a5eceac82eda57272e1bd2f0
hienjoy <wisdom16no@naver.com>
2024-12-02 12:44:27 +0900

    feat: 페이지 이동 경로 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { OutletContainer } from "./OutletContainer";
 import LoginPage from "./pages/loginPage/LoginPage";
 import HomePage from "./pages/homePage/HomePage";
 import SearchHomePage from "./pages/searchHomePage/SearchHomePage";
 import ContentPage from "./pages/contentPage/ContentPage";
+import SearchContentsPage from "./pages/searchContentsPage/SearchContentsPage";
+import GuestbookPage from "./pages/guestbookPage/GuestbookPage";
+import KnockHomePage from "./pages/knockHomePage/KnockHomePage";
 
 export default createBrowserRouter([
   {
     path: "/",
     element: <OutletContainer />,
     children: [
+      {
+        path: "/",
+        element: <Navigate to="/login" replace />,
+      },
       {
         path: "/login",
         element: <LoginPage />,
@@ -23,8 +30,20 @@ export default createBrowserRouter([
         element: <SearchHomePage />,
       },
       {
-        path: "/content/:category",
+        path: "/search/home/result",
+        element: <KnockHomePage />,
+      },
+      {
+        path: "/contents/:category/:homeId",
         element: <ContentPage />,
+      },
+      {
+        path: "/search/contents/:category",
+        element: <SearchContentsPage />,
+      },
+      {
+        path: "/guestbook/:homeId",
+        element: <GuestbookPage />,
       },
     ],
   },
