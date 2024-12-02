bf68b7e4c4d3afde63ac10761d51f82517c57168
hienjoy <wisdom16no@naver.com>
2024-12-02 12:43:50 +0900

    style: 화면 크기 설정

--- src/OutletContainer.jsx
+++ src/OutletContainer.jsx
@@ -1,14 +1,29 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./components/Header";
+import styled from "styled-components";
+
+const ViewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const OutletContent = styled.div`
+  flex: 1;
+  overflow: auto;
+`;
 
 export function OutletContainer() {
   const location = useLocation();
   const showHeader = location.pathname !== "/login";
 
   return (
-    <div>
+    <ViewContainer>
       {showHeader && <Header />}
-      <Outlet />
-    </div>
+      <OutletContent>
+        <Outlet />
+      </OutletContent>
+    </ViewContainer>
   );
 }
--- src/pages/loginPage/LoginPage.styled.jsx
+++ src/pages/loginPage/LoginPage.styled.jsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-
+  height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
 `;
